4e5f2dab7fdfbab4d166b2c040bbe0b88b635310
유정주 <dbwjdwn9216@gmail.com>
2024-06-29 20:15:49 +0900

    [정주] Section 2-1 완료

---
+++ 유정주/Landmarks/Landmarks/Model/ModelData.swift
@@ -0,0 +1,29 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by 유정주 on 6/29/24.
+//
+
+import Foundation
+
+var landmarks: [Landmark] = load("landmarkData.json")
+
+func load<T: Decodable>(_ filename: String) -> T {
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil) else {
+        fatalError("Couldn't find \(filename) in main bundle.")
+    }
+
+    let data: Data
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+
+    do {
+        return try JSONDecoder().decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
