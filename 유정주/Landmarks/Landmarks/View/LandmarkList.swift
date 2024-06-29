ceb82a96b8b46e0344263d93e07995bdef5e74b5
유정주 <dbwjdwn9216@gmail.com>
2024-06-29 20:39:30 +0900

    [정주] Section 2-4 완료

---
+++ 유정주/Landmarks/Landmarks/View/LandmarkList.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by 유정주 on 6/29/24.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
