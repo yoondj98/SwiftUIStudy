70e93d1605beba0a157cb4e152e115e6a6b26843
유정주 <dbwjdwn9216@gmail.com>
2024-06-29 20:38:09 +0900

    [정주] Section 2-2,3 완료

--- 유정주/Landmarks/Landmarks/Model/Landmark.swift
+++ 유정주/Landmarks/Landmarks/Model/Landmark.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SwiftUI
+import CoreLocation
 
 struct Landmark: Hashable, Codable {
     
---
+++ 유정주/Landmarks/Landmarks/View/LandmarkRow.swift
@@ -0,0 +1,31 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by 유정주 on 6/29/24.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview("Turtle Rock") {
+    Group {
+        LandmarkRow(landmark: landmarks[0])
+        LandmarkRow(landmark: landmarks[1])
+    }
+}
