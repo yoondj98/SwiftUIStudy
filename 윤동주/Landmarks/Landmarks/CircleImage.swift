287eba1e45f03790ea1207c573a32cb5f7d4f705
Xerath <yoondj98@naver.com>
2024-07-02 02:03:52 +0900

    [동주] complete Ch.1-1/Section 4

---
+++ 윤동주/Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by 윤동주 on 7/2/24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay { // 잘린 면을 기준으로 inset, offset으로 2씩 stroke 차지
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+        
+    }
+}
+
+#Preview {
+    CircleImage()
+}
---
+++ 윤동주/SwiftUI Tutorial 학습.md
@@ -0,0 +1,5 @@
+# SwiftUI Tutorial
+## Chapter 1
+
+- Preview 하단의 Live mode -> Selectable mode로 변경 후, Command + Control을 누른 채 Preview를 클릭 시 Embed in Stack, Grouping, modifier 반영 등이 가능함.
+- Image 사진명에 배율을 포함 시 'imageName@2x'처럼 써줌.
