d5c2c0301a2d40555b6299aebc7885bac64cfb73
Xerath <yoondj98@naver.com>
2024-07-02 02:32:49 +0900

    [동주] complete Ch.1-2/Section 1

---
+++ 윤동주/Landmarks/Landmarks/Model/Landmark.swift
@@ -0,0 +1,34 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by 윤동주 on 7/2/24.
+//
+
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: coordinates.latitude,
+                               longitude: coordinates.longitude)
+    }
+}
+
+struct Coordinates: Hashable, Codable {
+    var latitude: Double
+    var longitude: Double
+}
