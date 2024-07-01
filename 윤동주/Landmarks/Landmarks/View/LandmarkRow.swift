53b1dcded741dd1f209c1a6de8b5ae425982f0b3
Xerath <yoondj98@naver.com>
2024-07-02 02:35:04 +0900

    [동주] complete Ch.1-2/Section 2

---
+++ 윤동주/Landmarks/Landmarks/View/LandmarkRow.swift
@@ -0,0 +1,28 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by 윤동주 on 7/2/24.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkRow(landmark: landmarks[0])
+}
