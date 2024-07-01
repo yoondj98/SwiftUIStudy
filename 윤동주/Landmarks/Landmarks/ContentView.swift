bfd3472e4a7cec97022cbc7a912c72351da29fac
Xerath <yoondj98@naver.com>
2024-07-02 01:29:32 +0900

    [동주] init SwiftUI Tutorial

---
+++ 윤동주/Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 윤동주 on 7/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 윤동주/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 윤동주 on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
