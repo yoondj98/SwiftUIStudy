771249eda8d91173c03a1fa3d744e7fb4278deb8
SeungHyun Hong <whi7ehyun@gmail.com>
2024-07-02 18:43:44 +0900

    [승현] Complete Section 6 - Set up navigation between list and detail

--- 홍승현/Landmarks/Landmarks/Views/ContentView.swift
+++ 홍승현/Landmarks/Landmarks/Views/ContentView.swift
@@ -9,37 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    VStack(spacing: 0) {
-      MapView()
-        .frame(height: 300)
-
-      CircleImage()
-        .frame(width: 300, height: 300)
-        .offset(y: -130)
-        .padding(.bottom, -130)
-
-      VStack(alignment: .leading) {
-        Text("WhiteHyun")
-          .font(.title)
-        HStack {
-          Text("A tallest person who I've never met.")
-          Spacer()
-          Text("Hello :)")
-        }
-        .font(.subheadline)
-        .foregroundStyle(.secondary)
-
-        Divider()
-
-        Text("About Cat")
-          .font(.title2)
-
-        Text("Descriptive text goes here.")
-      }
-      .padding()
-
-      Spacer()
-    }
+    LandmarkList()
   }
 }
 
