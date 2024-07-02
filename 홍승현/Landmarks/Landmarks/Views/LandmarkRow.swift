a8cb6e46ba57fe3b7d9c3ab2fd0ad887daa01b35
SeungHyun Hong <whi7ehyun@gmail.com>
2024-07-02 18:01:51 +0900

    [승현] Complete Section 3 - Customize the row preview

--- 홍승현/Landmarks/Landmarks/Views/LandmarkRow.swift
+++ 홍승현/Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -22,6 +22,18 @@ struct LandmarkRow: View {
   }
 }
 
-#Preview {
+#Preview("Turtle Rock") {
   LandmarkRow(landmark: landmarks[0])
+}
+
+#Preview("Salmon") {
+  LandmarkRow(landmark: landmarks[1])
+}
+
+#Preview("Group View") {
+  Group {
+    LandmarkRow(landmark: landmarks[0])
+    LandmarkRow(landmark: landmarks[1])
+  }
+  .background(.yellow)
 }
