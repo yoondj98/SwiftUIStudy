f60e8f619522be767cd0f1979d078c44d257efc7
유정주 <dbwjdwn9216@gmail.com>
2024-07-03 23:04:09 +0900

    [정주] 챕터3 Section 1-1, 2 완료

---
+++ 유정주/Landmarks/Landmarks/View/Categories/CategoryHome.swift
@@ -0,0 +1,31 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by 유정주 on 7/3/24.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    
+    @Environment(ModelData.self) var modelData
+    
+    var body: some View {
+        NavigationSplitView {
+            List {
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    Text(key)
+                }
+            }
+            .navigationTitle("Featured")
+        } detail: {
+            Text("Select a Landmark")
+        }
+    }
+}
+
+#Preview {
+    CategoryHome()
+        .environment(ModelData())
+}
