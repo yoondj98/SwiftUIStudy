b9b2003865d39b8ad4c7801c20f337da01d908e7
Xerath <yoondj98@naver.com>
2024-07-04 01:22:43 +0900

    [동주] complete Ch.2-2/Section 4

--- 윤동주/Landmarks/Landmarks/Hikes/HikeView.swift
+++ 윤동주/Landmarks/Landmarks/Hikes/HikeView.swift
@@ -1,30 +1,41 @@
 /*
-See the LICENSE.txt file for this sample’s licensing information.
-
-Abstract:
-A view displaying information about a hike, including an elevation graph.
-*/
+ See the LICENSE.txt file for this sample’s licensing information.
+ 
+ Abstract:
+ A view displaying information about a hike, including an elevation graph.
+ */
 
 import SwiftUI
+
+// 이런 식으로 transition을 빼내어서 적용할 수 있음.
+extension AnyTransition {
+    static var moveAndFade: AnyTransition {
+        // 나타날 때와 사라질 때의 transition 효과를 달리 줄 수 있음.
+        .asymmetric(
+            insertion: .move(edge: .leading).combined(with: .opacity),
+            removal: .scale.combined(with: .opacity)
+        )
+    }
+}
 
 struct HikeView: View {
     var hike: Hike
     @State private var showDetail = false
-
+    
     var body: some View {
         VStack {
             HStack {
                 HikeGraph(hike: hike, path: \.elevation)
                     .frame(width: 50, height: 30)
-
+                
                 VStack(alignment: .leading) {
                     Text(hike.name)
                         .font(.headline)
                     Text(hike.distanceText)
                 }
-
+                
                 Spacer()
-
+                
                 Button {
                     withAnimation {
                         showDetail.toggle()
@@ -38,9 +49,10 @@ struct HikeView: View {
                         .padding()
                 }
             }
-
+            
             if showDetail {
                 HikeDetail(hike: hike)
+                    .transition(.moveAndFade) //나타나는 방식 설정
             }
         }
     }
