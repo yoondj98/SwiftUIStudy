acfa1e5f62581ebb191fbcf0d6f20791ebcf928a
SeungHyun Hong <whi7ehyun@gmail.com>
2024-07-04 16:19:14 +0900

    [승현] Complete Section 3 - Create a category row

---
+++ 홍승현/Landmarks/Landmarks/Views/Categories/CategoryItem.swift
@@ -0,0 +1,30 @@
+//
+//  CategoryItem.swift
+//  Landmarks
+//
+//  Created by 홍승현 on 7/4/24.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+
+  let landmark: Landmark
+
+  var body: some View {
+    VStack(alignment: .leading) {
+      landmark.image
+        .resizable()
+        .frame(width: 155, height: 155)
+        .clipShape(RoundedRectangle(cornerRadius: 5))
+
+      Text(landmark.name)
+        .font(.caption)
+    }
+    .padding(.leading, 15)
+  }
+}
+
+#Preview {
+  CategoryItem(landmark: ModelData().landmarks[0])
+}
